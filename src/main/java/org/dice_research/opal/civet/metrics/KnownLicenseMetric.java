e92d45dc85172a9283572a6e64871766d7c8c5c6
Gourab Sahu <gourab@mail.uni-paderborn.de>
2019-11-22 18:34:49 +0100

    BugFix: KnownLicense Metric
    1. Rights/Licenses in Dataset was not being detected. Its fixed.
    
    2. If Rights and License keyword present then should be aggregated. Which was not the case before. Its fixed now.
    
    3. Scoring approah changed as per Adrian's suggestions. If less than 100% license than 4 stars.

--- src/main/java/org/dice_research/opal/civet/metrics/KnownLicenseMetric.java
+++ src/main/java/org/dice_research/opal/civet/metrics/KnownLicenseMetric.java
@@ -13,6 +13,7 @@ import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.vocabulary.DCAT;
 import org.apache.jena.vocabulary.DCTerms;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dice_research.opal.civet.Metric;
@@ -25,8 +26,7 @@ import org.dice_research.opal.common.vocabulary.Opal;
  * @author Adrian Wilke
  */
 public class KnownLicenseMetric implements Metric {
-	
-	
+
 	public static boolean isValidLicenseURL(String LicenseURL) {
 		/*
 		 * new URL tries to create a new URL with provided license URL. If invalid
@@ -45,29 +45,29 @@ public class KnownLicenseMetric implements Metric {
 	private static final Logger LOGGER = LogManager.getLogger();
 	private static final String DESCRIPTION = "Extract License/rights information from dataset or it's distributions "
 			+ "If dataset has rights/license info then award 5 star "
-			+ "Else If dataset has no rights/license but more than 80% of dataset's distribution has then give 5 star"
-			+ "Else If dataset has no rights/license but less than 80% and more than 60% dataset's distribution has then 4 stars are awarded"
+			+ "Else If dataset has no rights/license but more than 100% of dataset's distribution has then give 5 star"
+			+ "Else If dataset has no rights/license but less than 100% and more than 60% dataset's distribution has then 4 stars are awarded"
 			+ "Else If dataset has no rights/license but less than 60% and more than 40% dataset's distribution has then 3 stars are awarded"
 			+ "Else If dataset has no rights/license but less than 40% and more than 20% dataset's distribution has then 2 stars are awarded"
 			+ "Else If dataset has no rights/license but less than 20% and more than  0% dataset's distribution has then 1 stars are awarded"
 			+ "Else if no License at all then 0 star is awarded.";
-
 
 	@Override
 	public Integer compute(Model model, String datasetUri) throws Exception {
 
 		LOGGER.info("Processing dataset " + datasetUri);
 
-		//Resource dataset = ResourceFactory.createResource(datasetUri);
+		// Resource dataset = ResourceFactory.createResource(datasetUri);
 
-		//NodeIterator nodeIterator = model.listObjectsOfProperty(dataset, DCAT.keyword);
+		// NodeIterator nodeIterator = model.listObjectsOfProperty(dataset,
+		// DCAT.keyword);
 
-		//For Score calculation
+		// For Score calculation
 		int TotalDistributions = 0;
 		int TotalDistributionsWithLicense = 0;
 		int TotalDistributionsWithRights = 0;
-		
-		//score to return
+
+		// score to return
 		int score = 0;
 
 		/*
@@ -75,24 +75,23 @@ public class KnownLicenseMetric implements Metric {
 		 * check anything with 'right'.
 		 */
 		int NumberOfLicensesInDataset = 0;
-		StmtIterator LicenseIterator = model
-				.listStatements(new SimpleSelector(null, DCTerms.license, (RDFNode) null));
-		if (LicenseIterator.hasNext()) {
-			while (LicenseIterator.hasNext()) {
-				Statement StatementWithLicense = LicenseIterator.next();
-				// If dct:license contains empty object then let NumberOfLicenseInDataset be 0.
-				if (StatementWithLicense.getObject().toString().isEmpty())
-					;
-				else {
-					// If at least one non-empty object found then check if it is a valid URI.
-					if (isValidLicenseURL(StatementWithLicense.getObject().toString())) {
-						NumberOfLicensesInDataset++;
-						break;
-					}
-				}
+		StmtIterator DatasetIterator = model.listStatements(new SimpleSelector(null, RDF.type, DCAT.Dataset));
+		if (DatasetIterator.hasNext()) {
+			Statement DataSetSentence = DatasetIterator.nextStatement();
+			Resource DataSet = DataSetSentence.getSubject();
+			// System.out.println(DataSet.toString());
+			if (DataSet.hasProperty(DCTerms.license)
+					&& !(DataSet.getProperty(DCTerms.license).getObject().toString().isEmpty())) {
+				if (isValidLicenseURL(DataSet.getProperty(DCTerms.license).getObject().toString()))
+					NumberOfLicensesInDataset++;
+			} else if (DataSet.hasProperty(DCTerms.rights)
+					&& !(DataSet.getProperty(DCTerms.rights).getObject().toString().isEmpty())) {
+				if (isValidLicenseURL(DataSet.getProperty(DCTerms.rights).getObject().toString()))
+					NumberOfLicensesInDataset++;
 			}
+		}
 
-		} else {
+		if (NumberOfLicensesInDataset == 0) {
 			// This means there is no license/rights info in dct:dataset, we need to check
 			// each dct:distributions
 
@@ -114,11 +113,10 @@ public class KnownLicenseMetric implements Metric {
 				}
 			}
 
-			if (TotalDistributionsWithLicense == 0)
+			if (TotalDistributionsWithLicense == 0 || TotalDistributionsWithLicense != TotalDistributions)
 
 			{
-				//Reset Total no. of distributions
-				TotalDistributions = 0 ;
+
 				/*
 				 * If control came here means, there is no dct:license keyword in available
 				 * distributions. So last step is to check if dct:right keyword with Valid URI
@@ -128,7 +126,6 @@ public class KnownLicenseMetric implements Metric {
 
 				while (DistributionsIteratorRights.hasNext()) {
 
-					TotalDistributions++;
 					Resource Distribution = (Resource) DistributionsIteratorRights.nextNode();
 
 					if (Distribution.hasProperty(DCTerms.rights)) {
@@ -139,36 +136,33 @@ public class KnownLicenseMetric implements Metric {
 				}
 			}
 		}
-      
-		
-		if(NumberOfLicensesInDataset > 0)
-			 score = 5;
-		else
-		{
-			if(TotalDistributionsWithLicense==0 && TotalDistributionsWithRights==0)
+
+		if (NumberOfLicensesInDataset > 0)
+			score = 5;
+		else {
+			if (TotalDistributionsWithLicense == 0 && TotalDistributionsWithRights == 0)
 				score = 0;
-			else if(TotalDistributionsWithLicense > 0 || TotalDistributionsWithRights > 0) 
-			{
-				 
-				int EvaluationInPercentage = (TotalDistributionsWithLicense > 0)?((TotalDistributionsWithLicense * 100) / TotalDistributions)
-						: ((TotalDistributionsWithRights * 100) / TotalDistributions);
-				
-				if (EvaluationInPercentage >= 80)
+			else if (TotalDistributionsWithLicense > 0 || TotalDistributionsWithRights > 0) {
+				int TotalKnownLicenses = TotalDistributionsWithLicense + TotalDistributionsWithRights;
+
+				int EvaluationInPercentage = ((TotalKnownLicenses * 100) / TotalDistributions);
+
+				if (EvaluationInPercentage == 100)
 					score = 5;
-				else if (EvaluationInPercentage >= 60) 
+				else if (EvaluationInPercentage < 100 && EvaluationInPercentage >= 60)
 					score = 4;
-				else if (EvaluationInPercentage >= 40) 
+				else if (EvaluationInPercentage >= 40)
 					score = 3;
-				else if ( EvaluationInPercentage >= 20) 
+				else if (EvaluationInPercentage >= 20)
 					score = 2;
-				else if ( EvaluationInPercentage > 0)
+				else if (EvaluationInPercentage >= 0)
 					score = 1;
-				else 
+				else
 					score = 0;
 			}
 		}
-		
-        return score;
+
+		return score;
 	}
 
 	@Override
--- src/test/java/org/dice_research/opal/civet/metrics/KnownLicenseMetricTest.java
+++ src/test/java/org/dice_research/opal/civet/metrics/KnownLicenseMetricTest.java
@@ -20,7 +20,19 @@ public class KnownLicenseMetricTest {
 
 	TestData testdata;
 
-	private static final String TEST_EDP_ICE = "Govdata-Allermoehe.ttl";
+	// Dataset has 5 distributions and all 5 distribution have rights/license info ---> 5 Stars
+	private static final String TestCase1 = "TestCaseForKnownLicense5stars_5dist_5rights.ttl";
+
+	// Dataset has 5 distributions and all 4 distribution have rights/license info ----> 4 stars
+	private static final String TestCase2 = "TestCaseForKnownLicense4stars_5dist_4rights.ttl";
+
+	// Dataset has direct rghts/license info in Dataset only -----> 5 stars
+	private static final String TestCase3 = "TestCaseForKnownLicense5stars_dataset_has_licenseInfo.ttl";
+	
+	// Out of 5 distributions 2 dist has licenses and 2 dist has rights -----> 4 stars
+	private static final String TestCase4 = "TestCaseForKnownLicense4stars_5dist_2license_2rights.ttl";
+	
+	
 	private static final String TEST_EDP_ICE_DATASET = "http://projekt-opal.de/dataset/http___europeandataportal_eu_set_data__3dff988d_59d2_415d_b2da_818e8ef3111701";
 
 	@Before
@@ -30,29 +42,32 @@ public class KnownLicenseMetricTest {
 	
 	
 	@Test
-	public void testGov() throws Exception {
-		
-		// Compute stars
+	public void TestCase1() throws Exception {
+
 		KnownLicenseMetric metric = new KnownLicenseMetric();
-		
-		//It is supposed to pass with 5 stars as dcat:Dataset has license information
-		Integer stars = metric.compute(testdata.getModel(TEST_EDP_ICE), TEST_EDP_ICE_DATASET); 
-		Assert.assertEquals("KnownLicense Test for Govdata-Allermoehe", 5, stars.intValue());
-		
+		Integer stars = metric.compute(testdata.getModel(TestCase1), TEST_EDP_ICE_DATASET);
+		Assert.assertEquals("5 Distributions and all have license ", 5, stars.intValue());
 	}
 
+	@Test
+	public void TestCase2() throws Exception {
+		KnownLicenseMetric metric = new KnownLicenseMetric();
+		Integer stars = metric.compute(testdata.getModel(TestCase2), TEST_EDP_ICE_DATASET);
+		Assert.assertEquals("Out of 5 distributions, 4 distributions have rights info", 4, stars.intValue());
+	}
 
 	@Test
-	public void testEdpIce() throws Exception {
-		
-		// Compute stars
+	public void TestCase3() throws Exception {
 		KnownLicenseMetric metric = new KnownLicenseMetric();
-		
-		//It is supposed to fail as it has only 50% of license information
-		Integer stars = metric.compute(testdata.getModel("Europeandataportal-Iceland.ttl"), TEST_EDP_ICE_DATASET); 
-		Assert.assertEquals("KnownLicense Test for EuropeanDataportal Iceland", 5, stars.intValue());
-		
-		
+		Integer stars = metric.compute(testdata.getModel(TestCase3), TEST_EDP_ICE_DATASET);
+		Assert.assertEquals("Dataset has license info directly", 5, stars.intValue());
+	}
+	
+	@Test
+	public void TestCase4() throws Exception {
+		KnownLicenseMetric metric = new KnownLicenseMetric();
+		Integer stars = metric.compute(testdata.getModel(TestCase4), TEST_EDP_ICE_DATASET);
+		Assert.assertEquals("Dataset 5 distributions, 2 dis has dct:license and 2 dis has dct:rights", 4, stars.intValue());
 	}
 
 }
