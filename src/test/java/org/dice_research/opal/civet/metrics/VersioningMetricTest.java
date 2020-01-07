b082b63d83fea5b7d3fb53f5d0018e28d684511f
vikrantsingh29 <singhvikrant29@gmail.com>
2020-01-07 07:57:27 +0100

    Changes test cases.
    Signed-off-by: vikrantsingh29 <singhvikrant29@gmail.com>

--- src/test/java/org/dice_research/opal/civet/metrics/VersioningMetricTest.java
+++ src/test/java/org/dice_research/opal/civet/metrics/VersioningMetricTest.java
@@ -12,58 +12,15 @@ import org.junit.Test;
  */
 public class VersioningMetricTest {
 
-	TestData testdata;
+	private TestData testdata;
 
 	@Before
 	public void setUp() throws Exception {
 		testdata = new TestData();
 	}
 
-	/**
-	 * Tests all 3 cases.
-	 */
-	@Test
-	public void testCases() throws Exception {
-		VersionMetric metric = new VersionMetric();
-
-		Literal versionInfo = ResourceFactory.createPlainLiteral("versionInfo");
-
-		String datasetUri = "https://example.org/dataset";
-		Resource dataset = ResourceFactory.createResource(datasetUri);
-
-		Resource versionInConformTo = ResourceFactory.createResource
-				("https://example.org/topic");
-		Statement version = ResourceFactory.createStatement
-				(versionInConformTo, RDF.type, OWL.versionInfo);
-
-		Model model = ModelFactory.createDefaultModel();
-		model.add(dataset, RDF.type, DCAT.dataset);
-
-		//no version Information
-		Assert.assertNull("No Version Info", metric.compute(model, datasetUri));
-
-		//version information in access or download Url
-		model.addLiteral(dataset, DCAT.accessURL, versionInfo);
-		Assert.assertEquals("versionInfo in AccessUrl", 3,
-				metric.compute(model, datasetUri).intValue());
-
-		//version Info in download Url
-		model.remove(dataset, DCAT.accessURL, versionInfo);
-		model.addLiteral(dataset, DCAT.downloadURL, versionInfo);
-		Assert.assertEquals("versionInfo in downloadUrl", 3,
-				metric.compute(model, datasetUri).intValue());
-
-		//versionInfo in conformTo
-		model.remove(dataset, DCAT.downloadURL, versionInfo);
-		model.addLiteral(dataset, OWL.versionInfo, versionInfo);
-		Assert.assertEquals("versionInfo in versionInfoProperty", 5,
-				metric.compute(model, datasetUri).intValue());
-	}
-
-
 	@Test
 	public void test3Stars() throws Exception {
-		
 
 		String testFile = "Govdata-Allermoehe.ttl";
 		String testFileDataset =
@@ -89,7 +46,6 @@ public class VersioningMetricTest {
 		Integer stars = metric.compute(testdata.getModel(testFile),
 				testFileDataset);
 
-		//Expected 3
 		Assert.assertEquals(testFile, 4, stars.intValue());
 	}
 
@@ -104,12 +60,12 @@ public class VersioningMetricTest {
 		Integer stars = metric.compute(testdata.getModel(testFile),
 				testFileDataset);
 
-		//Expected 3
 		Assert.assertEquals(testFile, 5, stars.intValue());
 	}
 
 	@Test
 	public void testNullStars() throws Exception {
+
 		String testFile = "Europeandataportal-Iceland.ttl";
 		String testFileDataset = "http://projekt-opal.de/dataset/http___" +
 				"europeandataportal_eu_set_data__3dff988d_59d2_415d_b2da_" +
@@ -119,7 +75,6 @@ public class VersioningMetricTest {
 		Integer stars = metric.compute(testdata.getModel(testFile),
 				testFileDataset);
 
-		//Expected null
 		Assert.assertNull(testFile, stars);
 	}
 }
