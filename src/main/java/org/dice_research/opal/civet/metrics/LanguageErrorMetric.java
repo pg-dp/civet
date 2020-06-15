631bf3d620e7a9d1517d25b88f6ade1487c1df02
vikrantsingh29 <singhvikrant29@gmail.com>
2020-06-15 10:12:27 +0200

    Adds Test cases , Optimizes LanguageErrorMetric Class
    Signed-off-by: vikrantsingh29 <singhvikrant29@gmail.com>

--- src/main/java/org/dice_research/opal/civet/metrics/LanguageErrorMetric.java
+++ src/main/java/org/dice_research/opal/civet/metrics/LanguageErrorMetric.java
@@ -5,7 +5,6 @@ import org.apache.jena.vocabulary.DCTerms;
 import org.dice_research.opal.civet.Metric;
 import org.dice_research.opal.common.vocabulary.Opal;
 import org.languagetool.JLanguageTool;
-import org.languagetool.language.AmericanEnglish;
 import org.languagetool.language.BritishEnglish;
 import org.languagetool.language.French;
 import org.languagetool.language.GermanyGerman;
@@ -15,33 +14,42 @@ import java.util.List;
 /**
  * Checks the grammar of the description of the model and rate the model accordingly .
  * JLANGTOOl lib is used for the grammar checking .
- * 3 Languages are checked as of now ,
+ * 3 Languages are checked as of now  which are English , German and French,
  * but any number of languages can be added using this library.
+ * If the model has some kind of description with the language tag ,
+ * it has given 1 star else null with no description or no language Tag
+ * Scoring Scheme is calculated as error per description length. Refer {@code scoringScheme}
+ * for the further analysis of the scoring distribution
  * @author Vikrant Singh
-        */
+ */
 
 public class LanguageErrorMetric implements Metric {
 
     private static final String DESCRIPTION = "Computes a score based on the description " +
             "given and how well the description is given," +
-            " Works for english , german and french , " +
+            " Works for english , german and french " +
             "support for other languages could be added easily";
 
     @Override
     public Integer compute(Model model, String datasetUri) throws Exception, LiteralRequiredException {
 
         Resource dataset = ResourceFactory.createResource(datasetUri);
-        JLanguageTool langTool = new JLanguageTool(new AmericanEnglish());
+        JLanguageTool langTool ;
         String language = " ";
-        int score = 0;
+        int score  ;
 
         Statement statement = model.getProperty(dataset, DCTerms.description);
         if (statement == null) {
             return null;
         }
 
+        String description = statement.getObject().toString();
+        String[] wordsCount = description.split(" ");
+        double wordsCountLength = wordsCount.length;
+
         NodeIterator languageIterator = model.listObjectsOfProperty(dataset, DCTerms.description);
 
+        //get the language from language Tag
         while (languageIterator.hasNext()) {
             RDFNode languageNode = languageIterator.next();
             if (languageNode.isLiteral())
@@ -50,11 +58,7 @@ public class LanguageErrorMetric implements Metric {
                 return null;
         }
 
-        String dct_description = statement.getObject().toString();
-        dct_description.replaceAll(".", " ");
-        String[] wordsCount = dct_description.split(" ");
-        double wordsCountLength = wordsCount.length;
-
+        //makes the JLangTool Object for a particular Language
         switch (language.toLowerCase()) {
             case "en":
                 langTool = new JLanguageTool(new BritishEnglish());
@@ -65,23 +69,32 @@ public class LanguageErrorMetric implements Metric {
             case "de":
                 langTool = new JLanguageTool(new GermanyGerman());
                 break;
+            default:
+                return  null ;
         }
 
-        List<RuleMatch> matches = langTool.check(dct_description);
+        //makes a list of errors
+        List<RuleMatch> matches = langTool.check(description);
         double errorCount = matches.size();
         double errorPerDescription = errorCount / wordsCountLength;
 
-        if (errorPerDescription > 0.9)
-            score = 0;
-        else if (errorPerDescription > 0.75)
+        score = scoringScheme(errorPerDescription);
+        return score;
+    }
+
+    private int scoringScheme(double errorCount) {
+        //calculates the score
+        int score = 0 ;
+
+        if (errorCount > 0.75)
             score = 1;
-        else if (errorPerDescription > 0.5)
+        else if (errorCount > 0.5)
             score = 2;
-        else if (errorPerDescription > 0.25)
+        else if (errorCount > 0.25)
             score = 3;
-        else if (errorPerDescription > 0.05)
+        else if (errorCount > 0.05)
             score = 4;
-        else if (errorPerDescription >= 0)
+        else if (errorCount >= 0)
             score = 5;
 
         return score;
@@ -96,5 +109,4 @@ public class LanguageErrorMetric implements Metric {
     public String getUri() throws Exception {
         return Opal.OPAL_METRIC_LANGUAGE_ERRORS.getURI();
     }
-
 }
--- src/test/java/org/dice_research/opal/civet/metrics/LanguageErrorMetricTest.java
+++ src/test/java/org/dice_research/opal/civet/metrics/LanguageErrorMetricTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 
 public class LanguageErrorMetricTest {
 
-    final String TEST_FILE = "TestDataUpdateRateMetric.ttl";
+    final String TEST_FILE = "TestDataLanguageErrorMetric.ttl";
     TestData testdata;
     Model model;
 
@@ -23,63 +23,56 @@ public class LanguageErrorMetricTest {
     }
 
     @Test
-    public void test5Starts() throws Exception {
+    public void testStarOne() throws Exception {
 
-        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-                "europeandataportal_eu_set_data_agricultural_land_classification_" +
-                "detailed_post1988_survey_alcl16793";
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_agricultural_land_classification_detailed_post1988_survey_alcr00995";
+        LanguageErrorMetric metric = new LanguageErrorMetric();
+        Integer stars = metric.compute(model, TEST_DATASET);
+        Assert.assertEquals(TEST_FILE, 1, stars.intValue());
+    }
+
+    @Test
+    public void testStarTwo() throws Exception {
+
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_agricultural_land_classification_detailed_post1988_survey_alcr008951";
+        LanguageErrorMetric metric = new LanguageErrorMetric();
+        Integer stars = metric.compute(model, TEST_DATASET);
+        Assert.assertEquals(TEST_FILE, 2, stars.intValue());
+    }
+
+    @Test
+    public void testStarThree() throws Exception {
+
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_14f6d44d_c5d4_5225_96d9_f42a0879c8f1";
+        LanguageErrorMetric metric = new LanguageErrorMetric();
+        Integer stars = metric.compute(model, TEST_DATASET);
+        Assert.assertEquals(TEST_FILE, 3, stars.intValue());
+    }
+
+    @Test
+    public void testStarFour() throws Exception {
+
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_02b05847_1f02_d38a_bd41_6bdc832c94ca";
+        LanguageErrorMetric metric = new LanguageErrorMetric();
+        Integer stars = metric.compute(model, TEST_DATASET);
+        Assert.assertEquals(TEST_FILE, 4, stars.intValue());
+    }
+
+    @Test
+    public void testStarsFive() throws Exception {
+
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_agricultural_land_classification_detailed_post1988_survey_alcl16793";
         LanguageErrorMetric metric = new LanguageErrorMetric();
         Integer stars = metric.compute(model, TEST_DATASET);
         Assert.assertEquals(TEST_FILE, 5, stars.intValue());
     }
-//
-//    @Test
-//    public void testStarOne() throws Exception {
-//
-//        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-//                "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
-//        LanguageErrorMetric metric = new LanguageErrorMetric();
-//        Integer stars = metric.compute(model, TEST_DATASET);
-//        Assert.assertEquals(TEST_FILE, 1, stars.intValue());
-//    }
-//
-//    @Test
-//    public void testStarTwo() throws Exception {
-//
-//        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-//                "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
-//        LanguageErrorMetric metric = new LanguageErrorMetric();
-//        Integer stars = metric.compute(model, TEST_DATASET);
-//        Assert.assertEquals(TEST_FILE, 2, stars.intValue());
-//    }
-//
-//    @Test
-//    public void testStarThree() throws Exception {
-//
-//        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-//                "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
-//        LanguageErrorMetric metric = new LanguageErrorMetric();
-//        Integer stars = metric.compute(model, TEST_DATASET);
-//        Assert.assertEquals(TEST_FILE, 3, stars.intValue());
-//    }
-//
-//    @Test
-//    public void testStarFour() throws Exception {
-//
-//        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-//                "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
-//        LanguageErrorMetric metric = new LanguageErrorMetric();
-//        Integer stars = metric.compute(model, TEST_DATASET);
-//        Assert.assertEquals(TEST_FILE, 4, stars.intValue());
-//    }
-//
-//    @Test
-//    public void testStarFive() throws Exception {
-//
-//        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-//                "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
-//        LanguageErrorMetric metric = new LanguageErrorMetric();
-//        Integer stars = metric.compute(model, TEST_DATASET);
-//        Assert.assertEquals(TEST_FILE, 5, stars.intValue());
-//    }
+
+    @Test
+    public void testNoLanguageTag() throws Exception {
+
+        final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_029d4698_6c32_46a1_ae7b_89beb74f8b14";
+        LanguageErrorMetric metric = new LanguageErrorMetric();
+        Integer stars = metric.compute(model, TEST_DATASET);
+        Assert.assertNull(TEST_FILE, stars);
+    }
 }
